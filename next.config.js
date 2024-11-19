cbcf6e2fb7fea5fa1bb364c2acf3225db814b778
Adron Hall <adron@thrashingcode.com>
2024-11-18 18:13:24 -0800

    Trying to fix the redirect on login with this next.config.js setting.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'adrons-core-platform.vercel.app',
+          },
+        ],
+        destination: 'https://adron.tools/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
