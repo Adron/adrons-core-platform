54ccc24ce92e9357e5b42b53c1ca516098e52c32
Adron Hall <adron@thrashingcode.com>
2024-11-29 01:31:17 -0800

    Just getting rid of the hello world.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold mb-4">
-        Hello World!
+        Hello, welcome to Adron Tools!
       </h1>
       <p className="text-xl">
-        Welcome to Adron&apos;s Core Platform
+        You've found the Core Platform, which is a starting point for any and all the apps you might want to use that Adron Tools offers.
       </p>
     </main>
   );
